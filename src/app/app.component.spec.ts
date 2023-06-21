7a8647620d74ce68118f78564320f881354e4d64
Pedro J. Molina <pjmolina@gmail.com>
2023-06-21 20:21:12 +0200

    fix test. remove protractor and unused e2e

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+describe("AppComponent", async () => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+  });
+  it("should create the app", async () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
+  });
+  it(`should have as title 'app'`, async () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
+    expect(app.title).toEqual("app");
+  });
+  it("should render title in a h1 tag", async () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-forms!');
-  }));
+    expect(compiled.querySelector("h1").textContent).toContain("Form Samples");
+  });
 });
